b4f9fb80fa9a4d230433c21d66be32b80829360c
Artem Yarulin <artem@unity3d.com>
2015-08-10 09:42:21 +0300

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rp-transducers "0.0.3"
+(defproject rp-transducers "0.0.4"
   :description "A set of additional transducers inspired by reactive programming (specifically RxJS)"
   :url "https://github.com/artemyarulin/rp-transducers"
   :license {:name "Eclipse Public License"
